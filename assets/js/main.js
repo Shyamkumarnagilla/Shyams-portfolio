00b69d3baaa9de0b7ed547c47ad55369b3d206a8
Shyamkumarnagilla <nagillashyamkumar7@gmail.com>
2025-07-13 19:44:50 +0530

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -78,3 +78,11 @@ color.forEach((color) => {
         localStorage.setItem('hue', activeHue);
     };
 });
+
+document.addEventListener('mousemove', function(e) {
+  const cursor = document.querySelector('.custom-cursor');
+  if (cursor) {
+    cursor.style.left = e.clientX + 'px';
+    cursor.style.top = e.clientY + 'px';
+  }
+});
